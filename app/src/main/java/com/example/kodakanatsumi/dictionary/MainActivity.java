987c50288ca9c322dd41c588e1b21962c40d1f46
Natsu028 <orangemikan.028@gmail.com>
2017-11-08 20:23:52 +0900

    製作中…

--- app/src/main/java/com/example/kodakanatsumi/dictionary/MainActivity.java
+++ app/src/main/java/com/example/kodakanatsumi/dictionary/MainActivity.java
@@ -4,18 +4,21 @@ import android.content.SharedPreferences;
 import android.support.v4.content.SharedPreferencesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.TreeSet;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
     ArrayAdapter<String> adapter;
+    //リストに表示する時にはadapterを通さないと表示されない
 
     EditText wordEditText;
     EditText meanEditText;
@@ -25,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     TreeSet<String> wordSet;
     SharedPreferences pref;
     SharedPreferences.Editor editor;
+    //SharedPreferencesは倉庫
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,5 +46,33 @@ public class MainActivity extends AppCompatActivity {
         wordSet = new TreeSet<String>();
         pref = getSharedPreferences("dictionary", MODE_PRIVATE);
         editor = pref.edit();
+
+        editor.putString("technology", "科学技術");
+        editor.putString("develop", "開発する");
+        editor.commit();
+        //commit()は必ず入れる
+
+        wordSet.add("technology");
+        wordSet.add("develop");
+        //keyのみwordSetに保存する
+
+        //Mapに追加
+        for (String word : wordSet) {
+            //ShardPreferences内の日本語を取得
+            //検索機能のためMapに追加
+            this.hashMap.put(word, pref.getString(word, null));
+
+            //adapterに追加
+            adapter.add("[" + word + "]" + pref.getString(word, null));
+
+            //listに表示
+            listView.setAdapter(adapter);
+
+        }
+        public void add
+
+
     }
+
+
 }
