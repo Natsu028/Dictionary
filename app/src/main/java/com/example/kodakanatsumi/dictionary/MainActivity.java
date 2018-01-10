1bb30dda161149df6822579df7e3c50e6a7ea6b5
Natsu028 <orangemikan.028@gmail.com>
2018-01-11 00:18:08 +0900

    完了

--- app/src/main/java/com/example/kodakanatsumi/dictionary/MainActivity.java
+++ app/src/main/java/com/example/kodakanatsumi/dictionary/MainActivity.java
@@ -1,78 +1,1 @@
-package com.example.kodakanatsumi.dictionary;
-
-import android.content.SharedPreferences;
-import android.support.v4.content.SharedPreferencesCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.TreeSet;
-
-public class MainActivity extends AppCompatActivity {
-
-    ListView listView;
-    ArrayAdapter<String> adapter;
-    //リストに表示する時にはadapterを通さないと表示されない
-
-    EditText wordEditText;
-    EditText meanEditText;
-    EditText searchWordText;
-
-    HashMap<String, String> hashMap;
-    TreeSet<String> wordSet;
-    SharedPreferences pref;
-    SharedPreferences.Editor editor;
-    //SharedPreferencesは倉庫
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        listView = (ListView) findViewById(R.id.listview);
-        wordEditText = (EditText) findViewById(R.id.word);
-        meanEditText = (EditText) findViewById(R.id.mean);
-        searchWordText = (EditText) findViewById(R.id.searchword);
-
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
-
-        this.hashMap = new HashMap<>();
-        wordSet = new TreeSet<String>();
-        pref = getSharedPreferences("dictionary", MODE_PRIVATE);
-        editor = pref.edit();
-
-        editor.putString("technology", "科学技術");
-        editor.putString("develop", "開発する");
-        editor.commit();
-        //commit()は必ず入れる
-
-        wordSet.add("technology");
-        wordSet.add("develop");
-        //keyのみwordSetに保存する
-
-        //Mapに追加
-        for (String word : wordSet) {
-            //ShardPreferences内の日本語を取得
-            //検索機能のためMapに追加
-            this.hashMap.put(word, pref.getString(word, null));
-
-            //adapterに追加
-            adapter.add("[" + word + "]" + pref.getString(word, null));
-
-            //listに表示
-            listView.setAdapter(adapter);
-
-        }
-        public void add
-
-
-    }
-
-
-}
+package com.example.kodakanatsumi.dictionary;import android.content.SharedPreferences;import android.support.v4.content.SharedPreferencesCompat;import android.support.v7.app.AppCompatActivity;import android.os.Bundle;import android.view.View;import android.widget.ArrayAdapter;import android.widget.EditText;import android.widget.ListView;import android.widget.Toast;import java.util.ArrayList;import java.util.HashMap;import java.util.List;import java.util.TreeSet;public class MainActivity extends AppCompatActivity {    ListView listView;    ArrayAdapter<String> adapter;    //リストに表示する時にはadapterを通さないと表示されない    EditText wordEditText;    EditText meanEditText;    EditText searchWordText;    HashMap<String, String> hashMap;    TreeSet<String> wordSet;    SharedPreferences pref;    SharedPreferences.Editor editor;    //SharedPreferencesは倉庫    @Override    protected void onCreate(Bundle savedInstanceState) {        super.onCreate(savedInstanceState);        setContentView(R.layout.activity_main);        listView = (ListView) findViewById(R.id.listview);        wordEditText = (EditText) findViewById(R.id.word);        meanEditText = (EditText) findViewById(R.id.mean);        searchWordText = (EditText) findViewById(R.id.searchword);        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);        this.hashMap = new HashMap<>();        wordSet = new TreeSet<String>();        pref = getSharedPreferences("dictionary", MODE_PRIVATE);        editor = pref.edit();        editor.putString("technology", "科学技術");        editor.putString("develop", "開発する");        editor.putString("apps", "アプリ");        editor.commit();        //commit()は必ず入れる        wordSet.add("technology");        wordSet.add("develop");        wordSet.add("apps");        //keyのみwordSetに保存する        wordSet.addAll(pref.getStringSet("wordSet", wordSet));        //Mapに追加        for (String word : wordSet) {            //ShardPreferences内の日本語を取得            //検索機能のためMapに追加            this.hashMap.put(word, pref.getString(word, null));            //adapterに追加            adapter.add("[" + word + "]" + pref.getString(word, null));        }        //listに表示        listView.setAdapter(adapter);    }    public void add(View v){        String enterWord = wordEditText.getText().toString(); //toString()はString型に変換        String enterMean = meanEditText.getText().toString();        String enterObjext = "[" + enterWord + "]" + enterMean;        wordSet.add(enterWord);        editor.putString(enterWord, enterMean);        editor.putStringSet("wordSet", wordSet); //TreeSetは(”key”, value)が必要だから        editor.commit();        //commit()を忘れずに‥        adapter.add(enterObjext);    }    public void search(View v){        String seachWord = searchWordText.getText().toString();//seachWordに探せるようにする        //Setに保存した単語を取得        wordSet.addAll(pref.getStringSet("wordSet", wordSet));//addAllは"１つ１つを見る"という意味        //Mapに追加        for (String word : wordSet){            //SharedPreference内の日本語を取得            //検索機能のため            hashMap.put(word, pref.getString(word, null));        }        if (hashMap.containsKey(seachWord)){            Toast.makeText(this, hashMap.get(seachWord), Toast.LENGTH_LONG).show();//見つかったら        }else {            Toast.makeText(this, "この単語は登録されていません", Toast.LENGTH_LONG).show();//もしも登録されていなかったら        }    }    public void delete(View v){            }}
